49feb80337f9a8448602f24e8389acef662901b9
Joshua <mramos29@ucol.mx>
2021-08-27 16:31:53 -0500

    primer commit

---
+++ carrera.js
@@ -0,0 +1,16 @@
+class Carrera{
+    constructor(){
+        this.activitionBlizzard = 0;
+        this.ubisoftSingapur = 0;
+        this.otraDemanda = 100;
+    }
+
+    dado(){
+        return Math.floor(Math.random()*6)+1;
+    }
+
+    farmeoDeAcosos(){
+        
+    }
+}
+new Carrera;
