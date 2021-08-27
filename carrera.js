7f13de4c456681236bc5ac9721fa9ceb1005c876
Joshua <mramos29@ucol.mx>
2021-08-27 16:57:25 -0500

    preparativos

--- carrera.js
+++ carrera.js
@@ -3,14 +3,21 @@ class Carrera{
         this.activitionBlizzard = 0;
         this.ubisoftSingapur = 0;
         this.otraDemanda = 100;
+        this.victimasBlizzard = new Array();
+        this.victimasUbisoft = new Array();
+
+        //console.log(this.dado());
     }
 
     dado(){
-        return Math.floor(Math.random()*6)+1;
+        let tirada = Math.floor(Math.random()*6)+1;
+        return tirada
     }
 
     farmeoDeAcosos(){
-        
+        for(let i = 0; i<this.otraDemanda; i++){
+            let empleadoPromedioB = this.dado();
+        }
     }
 }
 new Carrera;
